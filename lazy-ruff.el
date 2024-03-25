ec70f8d4813179ee62dd51024ec3a4baa9004a75
Christopher Buch Madsen <stopherbm@gmail.com>
2024-03-25 22:43:51 +0100

    Move comment to top

--- lazy-ruff.el
+++ lazy-ruff.el
@@ -36,6 +36,11 @@
 ;; Installation of 'ruff':
 ;; - Please refer to the 'ruff' documentation at:
 ;;   https://docs.astral.sh/ruff/installation/
+;;
+;; Configuring lazy-ruff:
+;; The global variables defined in the part before the ruff-lint-format-block
+;; function can be freely changed with setq in your init.el (or other
+;; personalization files) for configuring lazy-ruff.
 
 ;;; Code:
 
